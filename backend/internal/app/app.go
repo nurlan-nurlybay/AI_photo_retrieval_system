a89c5403a1a3b3f7a26bddc6d59c1257a010572a
neroframe <aidyn.kazhakhmet@nu.edu.kz>
2025-09-23 01:50:46 +0500

    feat: faiss support, and app struct completed

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -2,49 +2,66 @@ package app
 
 import (
 	"context"
-	"log"
+	"fmt"
+	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/nurlan-nurlybay/AI_photo_retrieval_system/config"
 	clipadapter "github.com/nurlan-nurlybay/AI_photo_retrieval_system/internal/adapter/clip"
 	faissadapter "github.com/nurlan-nurlybay/AI_photo_retrieval_system/internal/adapter/faiss"
 	postgresadapter "github.com/nurlan-nurlybay/AI_photo_retrieval_system/internal/adapter/postgres"
 	redisadapter "github.com/nurlan-nurlybay/AI_photo_retrieval_system/internal/adapter/redis"
+	httpdelivery "github.com/nurlan-nurlybay/AI_photo_retrieval_system/internal/delivery/http"
+
 	"github.com/nurlan-nurlybay/AI_photo_retrieval_system/internal/usecase"
+	"github.com/nurlan-nurlybay/AI_photo_retrieval_system/pkg/logger"
 )
 
 type App struct {
-	Redis  *redisadapter.Client
-	Clip   *clipadapter.Client
-	Faiss  *faissadapter.Client
-	PGRepo *postgresadapter.MediaRepository
+	Logger *logger.Logger
 
+	MediaRepo usecase.MediaRepo
+	Cache     usecase.Cache
+	VectorDB  usecase.VectorIndex
+	Embedder  usecase.Embedder
 	SearchUC *usecase.SearchUsecase
+
+	server   *http.Server
 	// Tracer trace.TracerProvider
 }
 
-func New(ctx context.Context, cfg *config.Config) (*App, error) {
-	redisClient := redisadapter.NewClient(cfg.Redis.Addr, cfg.Redis.Password, cfg.Redis.DB)
+func New(ctx context.Context, cfg *config.Config, log *logger.Logger) (*App, error) {
+	redisCache := redisadapter.NewClient(cfg.Redis.Addr, cfg.Redis.Password, cfg.Redis.DB)
 
 	clipClient := clipadapter.NewClient(cfg.Clip.BaseURL)
 
-	faissClient, err := faissadapter.NewClient(cfg.Faiss.Host, cfg.Faiss.Port)
-	if err != nil {
-		log.Fatalf("failed to connect to faiss: %v", err)
-	}
+	faissClient := faissadapter.NewClient(cfg.Faiss)
 
 	pgRepo, err := postgresadapter.NewMediaRepository(ctx, cfg.Postgres.DSN())
 	if err != nil {
-		log.Fatalf("failed to connect to postgres: %v", err)
+		log.Fatal("failed to connect to postgres: %v", err)
+		return nil, fmt.Errorf("connect postgres: %w", err)
 	}
 
 	searchUC := usecase.NewSearchUsecase(clipClient, faissClient, pgRepo)
 
-	return &App{Redis: redisClient,
-		Clip:     clipClient,
-		Faiss:    faissClient,
-		PGRepo:   pgRepo,
-		SearchUC: searchUC,
-		// Tracer:   trace.NewTracerProvider(),
+	v := validator.New()
+	searchHandler := httpdelivery.NewSearchHandler(searchUC, v, log)
+	router := httpdelivery.SetupRoutes(searchHandler)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", cfg.HTTP.Host, cfg.HTTP.Port),
+		Handler: router,
+	}
+
+	return &App{
+		Logger:    log,
+		MediaRepo: pgRepo,
+		Cache:     redisCache,
+		VectorDB:  faissClient,
+		Embedder:  clipClient,
+		SearchUC:  searchUC,
+		server:    srv,
 	}, nil
 }
 
